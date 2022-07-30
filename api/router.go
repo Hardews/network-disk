ea764b3f57b32382c76d9b9fbd5a17eb1f881986
Hardews <2021214014@stu.cqupt.edu.cn>
2022-07-30 17:25:18 +0800

    feat(api):新增分享文件功能

--- api/router.go
+++ api/router.go
@@ -18,14 +18,16 @@ func InitRouter() {
 		user.Use(middleware.JwtToken)
 		user.GET("/resource", getUserAllFile)      // 获取该用户的所有文件信息
 		user.PUT("/resource", updateFileAttribute) // 修改文件名或存储路径
-		user.GET("/share/:filename", shareFile)
+		user.GET("/share/:filename", shareFile)    // 分享文件
 	}
 
-	upload := engine.Group("/upload")
+	upload := engine.Group("")
 	{
 		upload.Use(middleware.JwtToken)
-		upload.POST("/", uploadFile) // 上传文件
-		upload.DELETE("/", delFile)  // 删除文件
+		upload.POST("/upload", uploadFile)                         // 上传文件
+		upload.DELETE("/upload", delFile)                          // 删除文件
+		engine.GET("/download_conn/:filename", downloadFileByConn) // 只能通过链接下载
+		engine.GET("/:username/:filename", downloadPublicFile)     // 公开的文件
 	}
 
 	engine.Run()
--- api/upload.go
+++ api/upload.go
@@ -1,11 +1,18 @@
 package api
 
 import (
+	"encoding/base64"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"io"
 	"log"
+	"net/http"
 	"network-disk/model"
 	"network-disk/service"
 	"network-disk/tool"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func delFile(ctx *gin.Context) {
@@ -16,14 +23,125 @@ func updateFileAttribute(ctx *gin.Context) {
 
 }
 
-func shareFile(ctx *gin.Context) {
+func downloadPublicFile(ctx *gin.Context) {
+	username := ctx.Param("username")
+	filename := ctx.Param("filename")
 
+	ur, err := service.GetUserResource(username, filename)
+	if err != nil {
+		if err == redis.Nil {
+			tool.RespErrorWithDate(ctx, "没有该文件")
+			return
+		}
+		log.Println(err)
+		tool.RespInternetError(ctx)
+		return
+	}
+
+	if ur.Permission != service.Public {
+		tool.RespErrorWithDate(ctx, "没有权限下载该文件")
+		return
+	}
+
+	downloadFile(ctx, filename, ur.ResourceName)
+}
+
+func downloadFileByConn(ctx *gin.Context) {
+	name := ctx.Param("filename")
+	str, err := base64.URLEncoding.DecodeString(name)
+	if err != nil {
+		log.Println(err)
+		tool.RespInternetError(ctx)
+		return
+	}
+
+	s := strings.Split(string(str), "-")
+	filename := s[0]
+	resource := s[1]
+
+	downloadFile(ctx, filename, resource)
+}
+
+func downloadFile(ctx *gin.Context, filename, resource string) {
+	file, err := os.Open(resource)
+	if err != nil {
+		log.Println(err)
+		tool.RespInternetError(ctx)
+		return
+	}
+	defer file.Close()
+
+	fileHeader := make([]byte, 512)
+	_, err = file.Read(fileHeader)
+	if err != nil {
+		log.Println(err)
+		tool.RespInternetError(ctx)
+		return
+	}
+
+	fileStat, err := file.Stat()
+	if err != nil {
+		log.Println(err)
+		tool.RespInternetError(ctx)
+		return
+	}
+
+	ctx.Writer.Header().Set("Content-Disposition", "attachment;filename="+filename)
+	ctx.Writer.Header().Set("Content-Type", http.DetectContentType(fileHeader))
+	ctx.Writer.Header().Set("Content-Length", strconv.FormatInt(fileStat.Size(), 10))
+
+	file.Seek(0, 0)
+
+	for {
+		var n int
+		tmp := make([]byte, 10) // 通过控制切片大小控制下载速度
+		n, err = file.Read(tmp)
+		if err == io.EOF {
+			return
+		}
+		ctx.Writer.Write(tmp[:n])
+	}
+}
+
+func shareFile(ctx *gin.Context) {
+	iUsername, _ := ctx.Get("username")
+	username := iUsername.(string)
+	filename := ctx.Param("filename")
+
+	ur, err := service.GetUserResource(username, filename)
+	if err != nil {
+		if err == redis.Nil {
+			tool.RespErrorWithDate(ctx, "没有该文件")
+			return
+		}
+		log.Println(err)
+		tool.RespInternetError(ctx)
+		return
+	}
+
+	var str string
+
+	switch ur.Permission {
+	case service.Public:
+		tool.RespSuccessfulWithDate(ctx, "http://127.0.0.1:8080/"+username+"/"+filename)
+	case service.Private:
+		tool.RespErrorWithDate(ctx, "分享失败，您以将该文件设置为仅自己可见")
+	case service.Permission:
+		// 使人们只能通过分享连接下载的想法是将url进行base64编码
+		str = base64.URLEncoding.EncodeToString([]byte(filename + "-" + ur.ResourceName))
+		tool.RespSuccessfulWithDate(ctx, "http://127.0.0.1:8080/download_conn/"+str)
+	}
 }
 
 func uploadFile(ctx *gin.Context) {
 	var user model.User
 	iUsername, _ := ctx.Get("username")
 	user.Username = iUsername.(string)
+
+	attribute := ctx.PostForm("attribute")
+	if attribute == "" {
+		attribute = service.Public
+	}
 
 	file, err := ctx.FormFile("file")
 	if err != nil {
