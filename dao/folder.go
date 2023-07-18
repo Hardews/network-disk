013ed064d680166b4d9e799425e516818ab4db2a
“liangminghai” <1225101127@qq.com>
2023-07-18 16:48:58 +0800

    fix folder create logic

--- dao/folder.go
+++ dao/folder.go
@@ -8,6 +8,7 @@ package dao
 
 import (
 	"network-disk/model"
+	"strconv"
 )
 
 func GetUsernameByFolderId(folderId uint) string {
@@ -22,13 +23,18 @@ func CreateFolder(folder model.Folder) (uint, error) {
 	var isFolderRepeat string
 	dB.Model(&model.Folder{}).Select("folder_name").Where("username = ? and folder_name = ?",
 		folder.Username, folder.FolderName).Scan(&isFolderRepeat)
+	// 一致则加个副本字段
 	if isFolderRepeat == folder.FolderName {
 		folder.FolderName = folder.FolderName + "_副本"
 	}
+
+	// Mysql 插入数据
 	err := dB.Create(&folder).Error
 	if err != nil {
 		return 0, err
 	}
+
+	// mysql 更新
 	err = dB.Create(&model.UserResources{
 		FolderId:   uint(folder.ParentFolder),
 		ResourceId: 0,
@@ -38,10 +44,21 @@ func CreateFolder(folder model.Folder) (uint, error) {
 	if err != nil {
 		return 0, err
 	}
+
+	// 查询 folder id
 	var folderId uint
 	err = dB.Model(&model.Folder{}).Select("id").
 		Where("username = ? and folder_name = ? and parent_folder = ?",
 			folder.Username, folder.FolderName, folder.ParentFolder).Scan(&folderId).Error
+	if err != nil {
+		return 0, err
+	}
+
+	// Redis 更新
+	key := strconv.Itoa(folder.ParentFolder) + ":folder"
+	urStr := "0&&0&&null"
+	_, err = rdb.HSet(redisStoragePrefix+folder.Username, key, urStr).Result()
+
 	return folderId, err
 }
 
